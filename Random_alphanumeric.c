432271fe40e26d875ffd02dd746e507fd3b55d1e
Shubham Samanta <59130080+ShubhamSamanta-1@users.noreply.github.com>
2022-10-25 10:35:31 +0530

    Create Random_alphanumeric.c

---
+++ Random_alphanumeric.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include <string.h>
+#include <time.h>
+int main()
+{
+    char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+    int l = strlen(a);
+    srand(time(NULL));
+    printf("%c\n", a[rand() % l]);
+    return 0;
+}
